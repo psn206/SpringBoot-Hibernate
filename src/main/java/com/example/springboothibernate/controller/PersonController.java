fae500441c5381dc0cfa24053bfefe45dc7d0671
Sergei Prutsakov <psn206@mail.ru>
2022-10-20 21:29:39 +0300

    add project JPA

--- src/main/java/com/example/springboothibernate/controller/PersonController.java
+++ src/main/java/com/example/springboothibernate/controller/PersonController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,11 +19,25 @@ public class PersonController {
 
     @GetMapping("/persons/by-city")
     public List<String> getPersonsByCity(@RequestParam String city) {
-
-        List<String> result = personRepository.getPersonsByCity(city).stream()
+        List<String> result = personRepository.findByCityOfLiving(city).stream()
                 .map(Person::toString)
                 .collect(Collectors.toList());
-
         return result;
+    }
+
+    @GetMapping("/persons/by-age")
+    public List<String> getPersonsByAge(@RequestParam int age) {
+        List<String> result = personRepository.findByAgeLessThanOrderByAgeDesc(age).stream()
+                .map(Person::toString)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+
+
+    @GetMapping("/persons/by-name-and-surname")
+    public Optional<Person>  getPersonsByName (@RequestParam String name, @RequestParam String surname) {
+        return   personRepository.findAllByNameAndSurname(name, surname);
+
     }
 }
--- src/main/java/com/example/springboothibernate/repository/PersonRepository.java
+++ src/main/java/com/example/springboothibernate/repository/PersonRepository.java
@@ -1,26 +1,18 @@
 package com.example.springboothibernate.repository;
 
 import com.example.springboothibernate.persons.Person;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
-
-@Repository
-@AllArgsConstructor
-public class PersonRepository {
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public List<Person> getPersonsByCity(String city) {
-        return entityManager.createQuery("select p from Person p where p.cityOfLiving = :city order by p.cityOfLiving", Person.class)
-                .setParameter("city", city)
-                .getResultList();
-    }
+import java.util.Optional;
 
 
+public interface PersonRepository extends JpaRepository <Person, Long>{
+
+    List<Person> findByCityOfLiving(String city);
+
+    List<Person> findByAgeLessThanOrderByAgeDesc(int age);
+
+
+    Optional<Person> findAllByNameAndSurname(String name, String surname);
 }
